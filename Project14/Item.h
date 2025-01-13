708af31f8cd68873295d013d29daf6331a306f9b
김현우 <hahewq99@naver.com>
2025-01-13 20:11:22 +0900

    Item 관련 사항 입니다

--- Project14/Item.h
+++ Project14/Item.h
@@ -1,39 +1,37 @@
 #ifndef ITEM_H
 #define ITEM_H
 
-#include <iostream>
 #include <string>
-#include "Charater.h"
-
 using namespace std;
 
-class Item {
-public:
+class Item
+{
+protected:
     string name;
-    int effect;
+    int price;
+    int healthAmount;
+    int damageAmount;
 
-    Item() {
-        if (rand() % 2 == 0) {
-            name = "체력 회복 아이템";
-            effect = 50; // 체력 회복
-        }
-        else {
-            name = "공격력 증가 아이템";
-            effect = 10; // 공격력 증가
-        }
-    }
+public:
+    Item();
+    Item(string name, int price, int healthAmount, int damageAmount);
 
-    void use(Character& player) {
-        if (name == "체력 회복 아이템") {
-            player.health += effect;
-            if (player.health > player.maxHealth) player.health = player.maxHealth;
-            cout << player.name << "이(가) 체력 회복 아이템을 사용하여 체력이 " << effect << "만큼 회복되었습니다." << endl;
-        }
-        else if (name == "공격력 증가 아이템") {
-            player.damage += effect;
-            std::cout << player.name << "이(가) 공격력 증가 아이템을 사용하여 공격력이 " << effect << "만큼 증가했습니다." << std::endl;
-        }
-    }
+    string getName();
+    int getPrice();
+    int getHealthAmount();
+    int getDamageAmount();
 };
 
-#endif
+class HealthPotion : public Item
+{
+public:
+    HealthPotion();
+};
+
+class DamageBoost : public Item
+{
+public:
+    DamageBoost();
+};
+
+#endif 
