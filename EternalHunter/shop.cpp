bb054e287b4251203784b4e2e3068c77939209a4
LeeDoHyun <ehugstjstod2003@gmail.com>
2025-01-13 20:01:39 +0900

    Shop.h 내 함수를 Shop.cpp로 구현부 분리

---
+++ EternalHunter/shop.cpp
@@ -0,0 +1,69 @@
+#include "shop.h"
+
+
+Shop::Shop() {}
+Shop::Shop(string name, vector<Item*> itemlist) : shopName(name), itemList(itemlist) {}
+
+
+void Shop::Shopping(Player* player)
+{
+	cout << shopName << "에 방문했습니다." << endl;
+	while (true)
+	{
+		int index = 0;
+		cin >> index;
+		switch (index)
+		{
+		case 1:
+			cout << "판매 목록 확인" << endl;
+			ShowItemlist();
+			break;
+		case 2:
+			cout << "구매할 아이템 인덱스를 입력해주세요" << endl;
+			cin >> index;
+			BuyItem(player, index);
+			break;
+		case 3:
+			ShowPlayerItemList(player);
+			cout << "판매할 아이템 인덱스를 입력해주세요" << endl;
+			cin >> index;
+			SellItem(player, index);
+			break;
+		case 4:
+			cout << "종료" << endl;
+			return;
+		default:
+			cout << "숫자를 잘못 입력하셨습니다." << endl;
+			break;
+		}
+	}
+}
+void Shop::ShowItemlist()
+{
+	for (int i = 0; i < itemList.size(); i++)
+	{
+		cout << i << ":" << itemList[i]->getName() << endl;
+	}
+}
+
+Item* Shop::BuyItem(Player * player, int index)
+{
+	if (player->getGold() > itemList[index]->getPrice())
+	{
+		player->modifyGold(-itemList[index]->getPrice());
+		return itemList[index];
+	}
+}
+void Shop::ShowPlayerItemList(Player* player)
+{
+	cout << "플레이어 아이템 목록" << endl;
+	for (int i = 0; i < player->getInventory().size(); i++)
+	{
+		cout << player->getInventory()[i] << endl;
+	}
+}
+void Shop::SellItem(Player* player, int index)
+{
+	player->modifyGold(player->getInventory().at(index)->getPrice());
+	player->removeItemFromPlayerInventory(index);
+}
--- EternalHunter/shop.h
+++ EternalHunter/shop.h
@@ -9,8 +9,8 @@ private:
 	string shopName;
 public:
 	Shop() {}
-	Shop(string name, vector<Item*> itemlist) : shopName(name), itemList(itemlist) {}
-	vector<Item*> itemList = {new HealthPotion, new DamageBoost };
+	Shop(string name, vector<Item*> itemlist);
+	vector<Item*> itemList = { new HealthPotion, new DamageBoost };
 	virtual ~Shop()
 	{
 		for (int i = 0; i < itemList.size(); i++)
@@ -20,71 +20,15 @@ public:
 	}
 	string getShopName(){return this->shopName;	}
 
-	void Shopping(Player* player)
-	{
-		cout << shopName << "에 방문했습니다." << endl;
-		while (true)
-		{
-			int index = 0;
-			cin >> index;
-			switch (index)
-			{
-			case 1:
-				cout << "판매 목록 확인" << endl;
-				ShowItemlist();
-				break;
-			case 2:
-				cout << "구매할 아이템 인덱스를 입력해주세요" << endl;
-				cin >> index;
-				BuyItem(player, index);
-				break;
-			case 3:
-				ShowPlayerItemList(player);
-				cout << "판매할 아이템 인덱스를 입력해주세요" << endl;
-				cin >> index;
-				SellItem(player, index);
-				break;
-			case 4:
-				cout << "종료" << endl;
-				return;
-			default:
-				cout << "숫자를 잘못 입력하셨습니다." << endl;
-				break;
-			}
-		}
-	}
+	void Shopping(Player* player);
 
-	void ShowItemlist()
-	{
-		for (int i = 0; i < itemList.size(); i++)
-		{
-			cout << i << ":" << itemList[i]->getName() << endl;
-		}
-	}
+	void ShowItemlist();
 
-	Item* BuyItem(Player* player, int index)
-	{
-		if (player->getGold() > itemList[index]->getPrice())
-		{
-			player->modifyGold(-itemList[index]->getPrice());
-			return itemList[index];
-		}
-	}
+	Item* BuyItem(Player* player, int index);
 
-	void ShowPlayerItemList(Player* player)
-	{
-		cout << "플레이어 아이템 목록" << endl;
-		for (int i = 0; i < player->getInventory().size(); i++)
-		{
-			cout << player->getInventory()[i] << endl;
-		}
-	}
+	void ShowPlayerItemList(Player* player);
 
-	void SellItem(Player* player,int index)
-	{
-		player->modifyGold(player->getInventory().at(index)->getPrice());
-		player->removeItemFromPlayerInventory(index);
-	}
+	void SellItem(Player* player, int index);
 };
 
 class NormalShop : public Shop
