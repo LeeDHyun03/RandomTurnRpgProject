72bda8cb8914ef04e63ce0027aef5bb48d2986bf
ArstralArsk <astralarsk@gmail.com>
2025-01-13 19:15:25 +0900

    Update main.cpp

--- EternalHunter/main.cpp
+++ EternalHunter/main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
 #include "main.h"
 
-int main()
-{
-	return 0;
+using namespace std;
+
+// 게임 시작 함수 정의
+void startGame() {
+    // 게임 매니저에서 게임 시작을 호출합니다.
+    GameManager::startGame();
+}
+
+int main() {
+    // 랜덤 시드 초기화
+    srand(static_cast<unsigned int>(time(0)));
+
+    // 게임 시작
+    startGame();
+
+    return 0;
 }
